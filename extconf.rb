84c674501820ee67bcbfbb1871a4d753033f8864
grant <grant@45b5d43f-8932-4c86-835a-3654e5842839>
2009-01-28 09:10:50 +0000

    - Improve Ingres library and header detection
    git-svn-id: http://code.ingres.com/ingres/drivers/ruby/main@1116 45b5d43f-8932-4c86-835a-3654e5842839

--- extconf.rb
+++ extconf.rb
@@ -1,21 +1,39 @@
+# UNIX/Linux/OS X
 # ruby -r mkmf extconf.rb  --with-ingres-include="${II_SYSTEM}/ingres/files/" --with-ingres-lib="${II_SYSTEM}/ingres/lib/"
-
+# Windows
+# ruby -r mkmf extconf.rb  --with-ingres-include="%II_SYSTEM%\ingres\files\" --with-ingres-lib="%II_SYSTEM%\ingres\lib\"
 #
-# if mkmf is not found on your system, install something like ruby1.8-dev
-#
-# May need to modify parameter to both "have_library" statements below
-# depending on operating system and/or version of Ingres.  See NOTEs
-# adjacent to each below.
+# If mkmf is not found on your system, install something like ruby1.8-dev
 #
 require 'mkmf'
 puts "Start extconf.rb"
-dir_config('ingres')
 
-if RUBY_PLATFORM !~ /mswin32/
-    if ENV["II_SYSTEM"]
-        puts "Using II_SYSTEM=#{ENV['II_SYSTEM']}"
-        $CFLAGS += " -I#{ENV["II_SYSTEM"]}/ingres/files"
+#Get the supplied library/header directory (if any)
+ingres_dirs=dir_config('ingres')
+
+#Sanity checks, --with-ingres-xxx override system set II_SYSTEM
+if ingres_dirs[0] =~ /files/ and ingres_dirs[1] =~ /lib/ 
+    puts "Using supplied directories:"
+    puts "Headers   - #{ingres_dirs[0]}"
+    puts "Libraries - #{ingres_dirs[1]}"
+
+elsif ENV["II_SYSTEM"] # fallback
+    puts "Using II_SYSTEM=#{ENV['II_SYSTEM']}"
+    $CFLAGS += " -I#{ENV["II_SYSTEM"]}/ingres/files"
+    if RUBY_PLATFORM !~ /mswin32/
+	libdir = ENV["II_SYSTEM"] + "/ingres/lib"
+        $LDFLAGS += " -L#{libdir}"
+        $LDFLAGS += " -lq.1 -lcompat.1 -lframe.1 -lrt -liiapi.1"
+        if RUBY_PLATFORM =~ /solaris/
+            $LDFLAGS += " -lsocket"
+        end
+    else
+	libdir = "#{ENV["II_SYSTEM"]}\\ingres\\lib"
+        $LIBPATH.push("-libpath:\"#{libdir}\"")
     end
+else
+    puts "Unable to proceed without the environment variable II_SYSTEM or the flags --with-ingres-files/--with-ingres-lib pointing to a valid Ingres installation"
+    exit(1)
 end
 
 if have_header('iiapi.h')
@@ -25,7 +43,7 @@ if have_header('iiapi.h')
             puts "Found Ingres 2.6 or earlier.\n"
         elsif have_library('iilibapi','IIapi_initialize')
             have_library('libingres')
-            puts "Found Ingres 2006 or later.\n"
+            puts "Found Ingres 2006/9.0.4 or later.\n"
         else
             puts "\n\nI couldn't find the Ingres libraries so I didn't create your Makefile.\n"
             puts "Do you have Ingres installed?\n"
@@ -35,15 +53,6 @@ if have_header('iiapi.h')
             exit
         end
     else
-        if ENV["II_SYSTEM"]
-            libdir = ENV["II_SYSTEM"] + "/ingres/lib"
-            $LDFLAGS += " -L#{libdir}"
-            $LDFLAGS += " -lq.1 -lcompat.1 -lframe.1 -lrt -liiapi.1"
-        end
-       
-        if RUBY_PLATFORM =~ /solaris/
-            $LDFLAGS += " -lsocket"
-        end
 
         if have_library('iiapi')
         else
