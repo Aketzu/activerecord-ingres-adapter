19f9bf1f5ff9736a68453ec7a40ec664580d9e2a
Félix Bellanger <felix.bellanger@gmail.com>
2014-10-13 16:49:33 +0200

    Implementing Ingres quirks in the visitor of arel (limit).

--- lib/arel/visitors/ingres.rb
+++ lib/arel/visitors/ingres.rb
@@ -2,6 +2,28 @@ module Arel
   module Visitors
     class Ingres < Arel::Visitors::ToSql
       private
+
+      def visit_Arel_Nodes_SelectStatement o, collector
+        collector << "SELECT "
+
+        if o.limit
+          collector << "FIRST #{o.limit} "
+        end
+
+        visit o.relation, collector
+
+        unless o.wheres.empty?
+          collector << " WHERE "
+          collector = inject_join o.wheres, collector, ' AND '
+        end
+
+        unless o.orders.empty?
+          collector << " ORDER BY "
+          collector = inject_join o.orders, collector, ', '
+        end
+
+        collector
+      end
     end
   end
 end
