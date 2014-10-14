9349efda001f2d736f25e3f5a029903a005247c7
Félix Bellanger <felix.bellanger@gmail.com>
2014-10-14 16:12:26 +0200

    Changed the Arel Visitor to make it compatible w/ recent versions of Arel.

--- lib/arel/visitors/ingres.rb
+++ lib/arel/visitors/ingres.rb
@@ -2,28 +2,28 @@ module Arel
   module Visitors
     class Ingres < Arel::Visitors::ToSql
       private
-      def visit_Arel_Nodes_SelectStatement o
+      def visit_Arel_Nodes_SelectStatement o, a
         [
           "SELECT",
-          (visit(o.limit) if o.limit),
-          o.cores.map { |x| visit_Arel_Nodes_SelectCore x }.join,
-          ("ORDER BY #{o.orders.map { |x| visit x}.join(', ')}" unless o.orders.empty?),
-          (visit(o.offset) if o.offset),
+          (visit(o.limit, a) if o.limit),
+          o.cores.map { |x| visit_Arel_Nodes_SelectCore x, a }.join,
+          ("ORDER BY #{o.orders.map { |x| visit x, a }.join(', ')}" unless o.orders.empty?),
+          (visit(o.offset a) if o.offset),
         ].compact.join ' '
       end
 
-      def visit_Arel_Nodes_SelectCore o
+      def visit_Arel_Nodes_SelectCore o, a
         [
-          "#{o.projections.map { |x| visit x }.join ', '}",
-          ("FROM #{visit(o.source)}" if o.source && !o.source.empty?),
-          ("WHERE #{o.wheres.map { |x| visit x }.join ' AND ' }" unless o.wheres.empty?),
-          ("GROUP BY #{o.groups.map { |x| visit x }.join ', ' }" unless o.groups.empty?),
-          (visit(o.having) if o.having),
+          "#{o.projections.map { |x| visit x, a }.join ', '}",
+          ("FROM #{visit(o.source, a)}" if o.source && !o.source.empty?),
+          ("WHERE #{o.wheres.map { |x| visit x, a }.join ' AND ' }" unless o.wheres.empty?),
+          ("GROUP BY #{o.groups.map { |x| visit x, a }.join ', ' }" unless o.groups.empty?),
+          (visit(o.having, a) if o.having),
         ].compact.join ' '
       end
 
-      def visit_Arel_Nodes_Limit o
-        "FIRST #{visit o.expr}"
+      def visit_Arel_Nodes_Limit o, a
+        "FIRST #{visit o.expr, a}"
       end
     end
   end
